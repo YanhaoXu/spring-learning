de1625eeb595cbf2df7491e226a21843039d138c
yanhao.xu <yanhao.xu@outlook.com>
2022-01-08 13:54:53 +0800

    feature(taco-cloud-simple): validate add

--- taco-cloud-simple/src/main/java/com/github/xuyh/tacocloud/controller/DesignTacoController.java
+++ taco-cloud-simple/src/main/java/com/github/xuyh/tacocloud/controller/DesignTacoController.java
@@ -6,10 +6,13 @@ import com.github.xuyh.tacocloud.pojo.Taco;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 
@@ -44,7 +47,10 @@ public class DesignTacoController {
 
 
   @PostMapping
-  public String processDesign(Taco design) {
+  public String processDesign(@Valid @ModelAttribute("design") Taco design, Errors errors) {
+    if (errors.hasErrors()) {
+      return "design";
+    }
     log.info("Process design:{}", design);
     return "redirect:/orders/current";
   }
