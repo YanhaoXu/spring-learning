e721be8903dcdabec8be4bce093cf8a4b8557f17
yanhao.xu <yanhao.xu@outlook.com>
2023-01-14 09:17:11 +0800

    pdai-tech init add

---
+++ pdai-tech/pdai-tech-springboot-hellodemo/src/main/java/tech/pdai/pdaitechspringboothellodemo/PdaiTechSpringbootHellodemoApplication.java
@@ -0,0 +1,13 @@
+package tech.pdai.pdaitechspringboothellodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdaiTechSpringbootHellodemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PdaiTechSpringbootHellodemoApplication.class, args);
+    }
+
+}
---
+++ pdai-tech/pdai-tech-springboot-hellodemo/src/test/java/tech/pdai/pdaitechspringboothellodemo/PdaiTechSpringbootHellodemoApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.pdai.pdaitechspringboothellodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdaiTechSpringbootHellodemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
